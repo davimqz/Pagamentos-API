6767781c3bf4c9a5f5b5f534e5a88c913217172e
Davi Marques <dmo3@cesar.school>
2025-05-14 21:21:14 -0300

    [Feat] - Adição do service.js

---
+++ pagamento-frontend/src/services/pagamentoService.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const API_URL = 'http:/localhost:8080/pagamentos';
+
+export const listarPagamentos = () => axios.get(API_URL);
+export const obterPagamentos = (id) => axios.get(`${API_URL}/${id}`);
+export const incluirPagamento = (dados) => axios.post(API_URL, dados);
+export const alterarPagamento = (id, dados) => axios.put(`${API_URL}/${id}`, dados);
+export const excluirPagamento = (id) => axios.delete(`${API_URL}/${id}`);
